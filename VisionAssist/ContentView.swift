3d0aaefda759b6329b2316847ac260dd2a1431a6
yunussyasar <yunusyasar.tr2114@gmail.com>
2025-12-04 18:09:29 +0300

    Initial Commit

---
+++ VisionAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionAssist
+//
+//  Created by Yunus Emre Yaşar on 4.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionAssist/VisionAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionAssistApp.swift
+//  VisionAssist
+//
+//  Created by Yunus Emre Yaşar on 4.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct VisionAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
